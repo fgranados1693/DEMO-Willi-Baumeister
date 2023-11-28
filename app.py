32e44ebabdd6c24a466db2e26b0c7ecfed0bf0bc
JesusEV <jesus.espinoza.val@gmail.com>
2023-11-28 20:00:51 +0100

    add image sections

--- app.py
+++ app.py
@@ -1,73 +1,65 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from shiny import ui, render, App
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from shiny import ui, render, App
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-
-t = np.linspace(0, 2 * np.pi, 1024)
-data2d = np.sin(t)[:, np.newaxis] * np.cos(t)[np.newaxis, :]
+n_grid = 10
 
 app_ui = ui.page_fixed(
-    ui.tags.head(
-        ui.tags.link(rel="stylesheet", href="https://bootswatch.com/4/sandstone/bootstrap.min.css")
+    ui.row(
+        ui.column(6, ui.input_radio_buttons("image_choice", "Choose an Image",
+                                             choices={"A.png": "A", "B.png": "B", "C.png": "C"},
+                                             selected="A")),
+        ui.column(6, ui.output_plot("original_image_plot")),
     ),
     ui.row(
-        ui.column(4,ui.input_radio_buttons("image_choice", "Choose an Image",
-            choices={"A.png": "A", "B.png": "B", "C.png": "C"}, selected="A")),
-        ui.column(4, ui.output_plot("original_image_plot")),   # Each plot in its own column
+        ui.row(ui.input_slider("xrange", "X range:", min=1, max=n_grid, step=1, value=0.)),
+        ui.row(ui.input_slider("yrange", "Y range:", min=1, max=n_grid, step=1, value=0.)),
+        ui.column(4, ui.output_plot("original_image_plot_with_section")),
+        ui.column(4, ui.output_plot("downsampled_image_plot")),
         ui.column(4, ui.output_plot("color_points_plot"))
     ),
     ui.row(
         ui.input_slider("obs", "Row:", min=0., max=1., value=0.5),
-        ui.output_plot("frequency_spectrum_plot")),
+        ui.output_plot("frequency_spectrum_plot"),
+    ),       
     ui.row(
-        ui.h2("Playing with colormaps"),
-        ui.layout_sidebar(
-        ui.panel_sidebar(
-            ui.input_radio_buttons("channel", "Channel (rgb)",
-                choices={"0": "R", "1": "G", "2": "B"}
-            ),
-            ui.input_slider("range", "Color range", 0, 255, value=(-1, 1), step=0.05),
-        ),
-        ui.panel_main(
-            ui.output_plot("plot")
-        )
-    ))
-)
+        ui.input_slider("threshold", "threshold:", min=0., max=1., value=0.5),
+        ui.column(4, ui.output_plot("red_channel_plot")),
+        ui.column(4, ui.output_plot("green_channel_plot")),   
+        ui.column(4, ui.output_plot("blue_channel_plot"))                   
+    )
+)    
 
-#app_ui = ui.page_fixed(
-#    ui.output_plot("original_image_plot"),
-#    ui.output_plot("downsampled_image_plot"),
-#    ui.output_plot("color_points_plot")
-#)
 
 def server(input, output, session):
     @output
     @render.plot
     def original_image_plot():
-        file_name = f"./{input.image_choice()}"
-        image = cv2.imread(file_name)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_rgb = load_image_rgb(input.image_choice())
         plt.imshow(image_rgb)
         plt.axis('off')
         plt.title("Original Image")
-        return plt.gcf()  # Return the current figure
+        return plt.gcf()
+
+    @output
+    @render.plot
+    def original_image_plot_with_section():
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        mark_section_on_image(image_rgb, x_range, y_range)
+        plt.imshow(image_rgb)
+        plt.axis('off')
+        plt.title("Original Image with Section")
+        return plt.gcf()
 
     @output
     @render.plot
     def downsampled_image_plot():
-        file_name = f"./{input.image_choice()}"
-        image = cv2.imread(file_name)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        scale_percent = 25  # percentage of original size
-        width = int(image.shape[1] * scale_percent / 100)
-        height = int(image.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        downsampled_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-        downsampled_image_rgb = cv2.cvtColor(downsampled_image, cv2.COLOR_BGR2RGB)
-
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        downsampled_image = image_rgb[y_range[0]:y_range[1], x_range[0]:x_range[1]]
         plt.imshow(downsampled_image)
         plt.axis('off')
         plt.title("Downsampled Image")
@@ -75,25 +67,24 @@ def server(input, output, session):
 
     @output
     @render.plot
+    def red_channel_plot():
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        channel_index = 0  # For green channel
+        threshold = 128  # Define your threshold here
+        channel = image_rgb[:, :, channel_index]
+        binary_channel = np.where(channel > threshold, 1, 0)
+        plt.imshow(binary_channel, plt.cm.gray)
+        plt.title(f'Channel {channel_index} after thresholding')
+        plt.axis('off')
+        return plt.gcf()
+
+    @output
+    @render.plot
     def color_points_plot():
-
-        file_name = f"./{input.image_choice()}"
-        image = cv2.imread(file_name)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        scale_percent = 25  # percentage of original size
-        width = int(image.shape[1] * scale_percent / 100)
-        height = int(image.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        downsampled_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-        downsampled_image_rgb = cv2.cvtColor(downsampled_image, cv2.COLOR_BGR2RGB)
-
-        # ... [PCA and scatter plot code]
-        # Step 2: Reshape the image to a 2D array of pixels
-        pixels = downsampled_image_rgb.reshape((-1, 3))
-        
-        # Optional: Reduce color dimensions for visualization
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        downsampled_image = image_rgb[y_range[0]:y_range[1], x_range[0]:x_range[1]]
+        pixels = downsampled_image.reshape((-1, 3))
         pca = PCA(n_components=2)
-        #pixels_reduced = pca.fit_transform(pixels)
         pixels_reduced = pca.fit_transform(pixels)
         plt.scatter(pixels_reduced[:, 0], pixels_reduced[:, 1], color=pixels / 255)
         plt.title("Color Points (Reduced to 2D)")
@@ -104,22 +95,15 @@ def server(input, output, session):
     @output
     @render.plot
     def frequency_spectrum_plot():
-        # Apply Fourier Transform
-        file_name = f"./{input.image_choice()}"
-        image = cv2.imread(file_name)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        downsampled_image = image_rgb[y_range[0]:y_range[1], x_range[0]:x_range[1]]
 
-        f = np.fft.fft2(image_rgb)
+        f = np.fft.fft2(downsampled_image)
         fshift = np.fft.fftshift(f)
-        #magnitude_spectrum = 20 * np.log(np.abs(fshift))
-
         center_row = int(input.obs() * fshift.shape[0])
         line_frequency_data = fshift[center_row, :]
-
-        # Calculate the magnitude
         magnitude_spectrum = np.abs(line_frequency_data)
         
-        # Plot the frequency spectrum along the line
         plt.plot(magnitude_spectrum)
         plt.ylim(1, 1e5)
         plt.yscale("log")
@@ -131,19 +115,74 @@ def server(input, output, session):
 
     @output
     @render.plot
-    def plot():
-        file_name = f"./{input.image_choice()}"
-        image = cv2.imread(file_name)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def red_channel_plot():
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        channel_index = 0  # Red channel
+        threshold = input.threshold()*255
+        binary_channel = apply_threshold(image_rgb, channel_index, threshold)
 
-        fig, ax = plt.subplots()
-        print(input.channel())
-        img = image_rgb[:, :, int(input.channel())]
-        print(np.min(image_rgb), np.min(image_rgb), image_rgb.shape)
-        im = ax.imshow(img, vmin=input.range()[0], vmax=input.range()[1])
-        fig.colorbar(im, ax=ax)
-        return fig
+        red_cmap = ListedColormap(['white', 'red'])
+        plt.imshow(binary_channel, cmap=red_cmap)
+        plt.title('Red Channel after Thresholding')
+        plt.axis('off')
+        return plt.gcf()
 
+    @output
+    @render.plot
+    def green_channel_plot():
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        channel_index = 1  # Green channel
+        threshold = input.threshold()*255
+        binary_channel = apply_threshold(image_rgb, channel_index, threshold)
+
+        green_cmap = ListedColormap(['white', 'green'])
+        plt.imshow(binary_channel, cmap=green_cmap)
+        plt.title('Green Channel after Thresholding')
+        plt.axis('off')
+        return plt.gcf()
+
+    @output
+    @render.plot
+    def blue_channel_plot():
+        image_rgb, x_range, y_range = load_and_slice_image(input)
+        channel_index = 2  # Blue channel
+        threshold = input.threshold()*255
+        binary_channel = apply_threshold(image_rgb, channel_index, threshold)
+
+        blue_cmap = ListedColormap(['white', 'blue'])
+        plt.imshow(binary_channel, cmap=blue_cmap)
+        plt.title('Blue Channel after Thresholding')
+        plt.axis('off')
+        return plt.gcf()
+
+def apply_threshold(image_rgb, channel_index, threshold):
+    channel = image_rgb[:, :, channel_index]
+    return np.where(channel > threshold, 1, 0)
+
+
+def load_image_rgb(filename):
+    image = cv2.imread(f"./{filename}")
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def load_and_slice_image(input):
+    image_rgb = load_image_rgb(input.image_choice())
+    img_length, img_width = image_rgb.shape[:2]
+    x_range, y_range = calculate_slice_range(input.xrange(), input.yrange(), img_length, img_width)
+    return image_rgb, x_range, y_range
+
+def calculate_slice_range(x_input, y_input, img_length, img_width):
+    x_range = (int((x_input - 1) / n_grid * img_width), int(x_input / n_grid * img_width))
+    y_range = (int((y_input - 1) / n_grid * img_length), int(y_input / n_grid * img_length))
+    return x_range, y_range
+
+def mark_section_on_image(image_rgb, x_range, y_range):
+    i_start, i_end = y_range
+    j_start, j_end = x_range
+    image_rgb[i_start:i_end, j_start-4:j_start+4] = 255
+    image_rgb[i_start:i_end, j_end-4:j_end+4] = 255
+    image_rgb[i_start-4:i_start+4, j_start:j_end] = 255
+    image_rgb[i_end-4:i_end+4, j_start:j_end] = 255
 
 
 app = App(app_ui, server)
+
